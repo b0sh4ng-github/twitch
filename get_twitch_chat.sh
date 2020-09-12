a5caa4b28656fb774b795436a304d958ade5eae3
b0sh4ng-github <b0sh4ng@gmail.com>
2020-09-12 10:43:06 -0400

    optimize

--- get_twitch_chat.sh
+++ get_twitch_chat.sh
@@ -3,8 +3,8 @@ mkdir "twitch_chat" > /dev/null 2>&1
 function get_twitch_chat {
     VID=$(<VID.txt)
     echo "I'm starting at video "$VID
-    MAX_T=1000
-    EVID=$(($VID-1000))
+    MAX_T=2500
+    EVID=$(($VID - 2500))
     while [ $VID -ge $EVID ]; do
         ((i=i%MAX_T)); ((i++==0)) && wait
         tcd --settings-file "custom_settings.json" -v $VID -f json -o "twitch_chat" &
